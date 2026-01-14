39d9f1e744c654895a7def2219cd9129935c9371
hogdanish <121070110+hogdanish@users.noreply.github.com>
2026-01-14 18:11:39 +0000

    Updated to latest godot-build releases

--- Casks/godot-dev@4.6-rc1.rb
+++ Casks/godot-dev@4.6-rc1.rb
@@ -1,21 +1,21 @@
-cask "godot-dev" do
+cask "godot-dev@4.6-rc1" do
   version "4.6-rc1"
   sha256 "461c1be2be7ccd7d1167a5d0afdbb93ba0ebc33f7fb8a65ef18aa256fbac118a"
 
   url "https://github.com/godotengine/godot-builds/releases/download/4.6-rc1/Godot_v4.6-rc1_macos.universal.zip",
       verified: "github.com/godotengine/godot-builds/"
-  name "Godot Engine (Latest)"
-  desc "Free and open source 2D and 3D game engine (Latest godot-builds release)"
+  name "Godot Engine (Build 4.6-rc1)"
+  desc "Free and open source 2D and 3D game engine (godot-builds release4.6-rc1)"
   homepage "https://godotengine.org/"
 
   livecheck do
-    url "https://github.com/godotengine/godot-builds/releases"
-    strategy :github_latest
-    regex(/^(\d+\.\d+-(dev|rc)\d+)$/)
+    skip "This is a versioned cask"
+    
+    
   end
 
   auto_updates true
-  conflicts_with cask: "godot-dev@*"
+  conflicts_with cask: "godot-dev"
 
   app "Godot.app", target: "Godot Dev.app"
 
