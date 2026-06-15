b500bb1924a3f4cd632041b5226037c1b7140d34
hogdanish <121070110+hogdanish@users.noreply.github.com>
2026-06-15 19:26:02 +0000

    Updated to latest godot-build releases

--- Casks/godot-dev@4.6.1-rc1.rb
+++ Casks/godot-dev@4.6.1-rc1.rb
@@ -1,11 +1,11 @@
-cask "godot-dev@4.6-dev5" do
-  version "4.6-dev5"
-  sha256 "2c6e833420786a6a0627c652026356f96177d0440576654bc09c5ea0661adcb8"
+cask "godot-dev@4.6.1-rc1" do
+  version "4.6.1-rc1"
+  sha256 "07009bb203a5145acdc5143761f98428eafda89e4f33d431168865defe83a6a6"
 
-  url "https://github.com/godotengine/godot-builds/releases/download/4.6-dev5/Godot_v4.6-dev5_macos.universal.zip",
+  url "https://github.com/godotengine/godot-builds/releases/download/4.6.1-rc1/Godot_v4.6.1-rc1_macos.universal.zip",
       verified: "github.com/godotengine/godot-builds/"
-  name "Godot Engine (Build 4.6-dev5)"
-  desc "Free and open source 2D and 3D game engine (godot-builds release 4.6-dev5)"
+  name "Godot Engine (Build 4.6.1-rc1)"
+  desc "Free and open source 2D and 3D game engine (godot-builds release 4.6.1-rc1)"
   homepage "https://godotengine.org/"
 
   livecheck do
--- Casks/godot-dev@4.7-rc3.rb
+++ Casks/godot-dev@4.7-rc3.rb
@@ -1,21 +1,19 @@
-cask "godot-dev" do
+cask "godot-dev@4.7-rc3" do
   version "4.7-rc3"
   sha256 "3a62023ed3bf5f0673cbf9e23648f400f94939e6904468075d49e45e8c70504d"
 
   url "https://github.com/godotengine/godot-builds/releases/download/4.7-rc3/Godot_v4.7-rc3_macos.universal.zip",
       verified: "github.com/godotengine/godot-builds/"
-  name "Godot Engine (Latest)"
-  desc "Free and open source 2D and 3D game engine (Latest godot-builds release)"
+  name "Godot Engine (Build 4.7-rc3)"
+  desc "Free and open source 2D and 3D game engine (godot-builds release 4.7-rc3)"
   homepage "https://godotengine.org/"
 
   livecheck do
-    url "https://github.com/godotengine/godot-builds/releases"
-    strategy :github_latest
-    regex(/^(\d+\.\d+(?:\.\d+)?-[a-z]+\d*)$/)
+    skip "This is a versioned cask"
   end
 
   auto_updates true
-  conflicts_with cask: "godot-dev@*"
+  conflicts_with cask: "godot-dev"
   depends_on :macos
 
   app "Godot.app", target: "Godot Dev.app"
