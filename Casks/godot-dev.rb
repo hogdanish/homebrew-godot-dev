0cbb43f4f1b3f58c16f895b3e169e8e1dd3a9083
hogdanish <121070110+hogdanish@users.noreply.github.com>
2026-07-01 18:46:28 +0000

    Updated to latest godot-build releases

--- Casks/godot-dev.rb
+++ Casks/godot-dev.rb
@@ -1,8 +1,8 @@
 cask "godot-dev" do
-  version "4.7-rc3"
-  sha256 "3a62023ed3bf5f0673cbf9e23648f400f94939e6904468075d49e45e8c70504d"
+  version "4.7.1-rc1"
+  sha256 "134ad58d3ebe4e504699fd65f00183bdc8fb952a7c53a2fe6fe0f3191fa3087f"
 
-  url "https://github.com/godotengine/godot-builds/releases/download/4.7-rc3/Godot_v4.7-rc3_macos.universal.zip",
+  url "https://github.com/godotengine/godot-builds/releases/download/4.7.1-rc1/Godot_v4.7.1-rc1_macos.universal.zip",
       verified: "github.com/godotengine/godot-builds/"
   name "Godot Engine (Latest)"
   desc "Free and open source 2D and 3D game engine (Latest godot-builds release)"
